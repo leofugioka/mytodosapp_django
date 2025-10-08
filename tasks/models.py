7f2e016b919839790a517f02a993628b82fd6ab1
Leandro Tsuneo Fugioka <77177218+leofugioka@users.noreply.github.com>
2025-10-08 09:49:54 -0300

    Initial commit

---
+++ tasks/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Título")
+    description = models.TextField(blank=True, null=True, verbose_name="Descrição")
+    is_completed = models.BooleanField(default=False, verbose_name="Concluído")
+    due_date = models.DateTimeField(auto_now=False, blank=True, null=True, verbose_name="Finalizar até")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Criado em")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Atualizado em")
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['is_completed', '-created_at']
+        verbose_name = "Tarefa"
+        verbose_name_plural = "Tarefas"
---
+++ tasks/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def task_list(request):
+    return render(request, 'tasks/task_list.html')
